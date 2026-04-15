eb00a3f2f87a26ddd44fea3e4c6a3542efb91210
Wansig29 <jcgarcia853@iskwela.psau.edu.ph>
2026-04-15 19:02:10 +0800

    Fix syntax error in database config

--- config/database.php
+++ config/database.php
@@ -62,12 +62,8 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
                 PDO::ATTR_EMULATE_PREPARES => true,
-<<<<<<< HEAD
                 PDO::ATTR_PERSISTENT => false,
                 PDO::ATTR_TIMEOUT => 30,
-=======
-                PDO::ATTR_PERSISTENT => env('DB_PERSISTENT', true),
->>>>>>> e5e8861400995d11bb44686391526f6e76f7b46e
             ]) : [],
         ],
 
